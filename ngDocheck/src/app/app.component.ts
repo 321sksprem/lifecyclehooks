61b2a3b28d9bc9b650826d0d19f24d6b30d0d2b6
321sksprem <Premkumar2000>
2024-02-29 18:27:39 +0530

    Added app file

--- ngDocheck/src/app/app.component.ts
+++ ngDocheck/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ngDocheck';
+ private parentData:{count:number} ={count:0}
+
+  constructor(){ }
+
+  ngOnInit(): void {
+
+  }
+
+
+  changeData(){
+
+  this.parentData.count = this.parentData.count + 1
+
+  }
 }
--- ngDocheck/src/app/child/child.component.ts
+++ ngDocheck/src/app/child/child.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,Input,SimpleChange,DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent {
+export class ChildComponent implements OnInit,DoCheck{
+
+@Input() parentData:{count:number} = {count:0}
+diff = undefined;
+
+ constructor(){}
+  ngOnInit(): void {
+   console.log("init")
+   this.diff=this.parentData.count;
+  }
+
+  ngDoCheck(): void {
+    console.log("diff",this.parentData.count)
+    this.diff = this.parentData.count
+}
+
+  ngOnChange(values:SimpleChange){
+     console.log("simplechanges",values);
+     this.diff = values ['parentData'].currentUser.count;
+  }
+
 
 }
